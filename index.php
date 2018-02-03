3d55c6a637e823341796884b27f7f1ad5b0ee140
WilliamTeixeira <tx.william@gmail.com>
2018-02-03 18:40:50 -0200

    Tela de Login e Logout

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -12,19 +13,51 @@ $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$Page = new Page();
+	$page = new Page();
 	
-	$Page->setTpl("index");
+	$page->setTpl("index");
 
 });
 
 $app->get('/admin', function() {
     
-	$Page = new PageAdmin();
+    User::verifyLogin();
+
+	$page = new PageAdmin();
 	
-	$Page->setTpl("index");
+	$page->setTpl("index");
 
 });
+
+$app->get('/admin/login', function() {
+    
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+	
+	$page->setTpl("login");
+
+});
+
+
+$app->post('/admin/login', function() {
+    
+    User::login($_POST["login"], $_POST["password"]);
+	
+    header("Location: /admin");
+    exit;
+
+});
+
+$app->get('/admin/logout', function() {
+    
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
+});
+
 
 $app->run();
 
